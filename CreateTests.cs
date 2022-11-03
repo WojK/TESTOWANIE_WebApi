1e6a57ef72d729cbb4964d23d44ae6d7d51f3d91
Rafał Sulowski <48453164+rafalsulowski@users.noreply.github.com>
2022-11-03 23:56:58 +0100

    rs test

---
+++ CreateTests.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using AutoFixture.Xunit2;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using TestowanieWebApi.Database;
+using TestowanieWebApi.Models;
+using TestowanieWebApi.Services;
+using Xunit;
+
+namespace Testowanie {
+    public class CreateTests {
+
+
+        [Theory, AutoData]
+        public void When_AddClient_ReturnsClientWithProperProperties(string Name, string Surname,
+            string Country) {
+
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO client = new ClientDTO {
+                Name = Name,
+                Surname = Surname,
+                Country = Country,
+                Age = 10,
+                Gender = "Female",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act
+            Client newClient = clientService.AddClient(client);
+
+
+            // assert
+            Assert.Equal(Name, newClient.Name);
+            Assert.Equal(Surname, newClient.Surname);
+            Assert.Equal(Country, newClient.Country);
+            Assert.Equal(client.Gender, newClient.Gender);
+            Assert.Equal(client.Age, newClient.Age);
+            Assert.Equal(client.Email, newClient.Email);
+
+        }
+
+
+        [Fact]
+        public void When_AddClient_WithIncorrectAge_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = -10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithNullName_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithNullSurname_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Name = "Jan",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithNullCountry_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithNullEmail_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Age = 10,
+                Gender = "Male",
+                Country = "Poland",
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithNullGender_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Age = 10,
+                Country = "Poland",
+                Email = "string@gmail.com"
+
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithIncorrectGender_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Age = 10,
+                Country = "Poland",
+                Gender = "Unknown",
+                Email = "string@gmail.com"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddClient_WithIncorrectEmail_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO newClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Age = 10,
+                Country = "Poland",
+                Gender = "Female",
+                Email = "someemail.123"
+            };
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClient); });
+        }
+
+        [Fact]
+        public void When_AddNullValue_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+            ClientDTO? newClientNull = null;
+
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+<<<<<<< HEAD
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.AddClient(newClientNull); });
+        }
+=======
+
+>>>>>>> 90401444eb41d083f086c0416ae15225515b272e
+    }
+}
---
+++ EditTests.cs
@@ -0,0 +1,306 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestowanieWebApi.Database;
+using TestowanieWebApi.Models;
+using TestowanieWebApi.Services;
+
+namespace Testowanie {
+    public class EditTests {
+
+        [Fact]
+        public void When_EditClient_ReturnsClientWithEditedProperties() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            Client client = new Client {
+                Name = "Janek",
+                Surname = "Kowal",
+                Country = "England",
+                Age = 12,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns(client);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 1;
+            ClientDTO clientDTO = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act
+            Client editedClient = clientService.EditClient(Id, clientDTO);
+
+            // assert
+            Assert.Equal(editedClient.Name, clientDTO.Name);
+            Assert.Equal(editedClient.Surname, clientDTO.Surname);
+            Assert.Equal(editedClient.Country, clientDTO.Country);
+            Assert.Equal(editedClient.Gender, clientDTO.Gender);
+            Assert.Equal(editedClient.Age, clientDTO.Age);
+        }
+
+        [Fact]
+        public void When_EditClient_WithIncorrectAgeProperty_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            Client client = new Client {
+                Name = "Janek",
+                Surname = "Kowal",
+                Country = "England",
+                Age = 12,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns(client);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 1;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = -10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithIncorrectID_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithNullName_RaisesException(){
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO
+            {
+                Name = null,
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithNullSurname_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = null,
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithNullCountry_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = null,
+                Age = 10,
+                Gender = "Male",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+        [Fact]
+        public void When_EditClient_WithNullGender_RaisesException()
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = null,
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithNullEmail_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Male"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+<<<<<<< HEAD
+        public void When_EditClient_WithNullClientButCorrectId_RaisesException()
+=======
+        public void When_EditClient_WithIncorrectEmail_RaisesException()
+>>>>>>> 90401444eb41d083f086c0416ae15225515b272e
+        {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+<<<<<<< HEAD
+            Client client = new Client
+            {
+                Name = "Janek",
+                Surname = "Kowal",
+                Country = "England",
+                Age = 12,
+                Gender = "Male"
+            };
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns(client);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 1;
+            ClientDTO editedClient = null;
+
+            // act and assert
+            Assert.Throws<ArgumentException>(
+=======
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Female",
+                Email = "someemail.21"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+
+        [Fact]
+        public void When_EditClient_WithIncorrectGender_RaisesException() {
+            // arrange
+            var clientsContextMock = new Mock<IClientContext>();
+
+            clientsContextMock.Setup(x => x.Find(It.IsAny<int>())).Returns((Client)null);
+            IClientService clientService = new ClientService(clientsContextMock.Object);
+
+
+            int Id = 123;
+            ClientDTO editedClient = new ClientDTO {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Country = "Poland",
+                Age = 10,
+                Gender = "Unknown",
+                Email = "string@gmail.com"
+            };
+
+            // act and assert
+            Assert.Throws<Exception>(
+>>>>>>> 90401444eb41d083f086c0416ae15225515b272e
+                () => { Client client = clientService.EditClient(Id, editedClient); });
+        }
+    }
+}
